19dfa87ca294c57f903af15f2c946d07ac48185d
Svendsen92 <ssven16@student.aau.dk>
2018-05-13 12:04:17 +0200

    Fixed Reprojection threshold
    Made the reprojection threshold dynamic.

--- opencv_image_stitching/Warping.cpp
+++ opencv_image_stitching/Warping.cpp
@@ -13,26 +13,31 @@ Mat Warping::warp(Mat image, MatchedKeyPoint features) {
 Mat Warping::perspective_warping_(Mat &img) {
 	cout << "Perspective() {" << endl;
 
-	//double threshold = (num_features_ / 25) - 1;
-	//double iterations = (0.9893 * pow(threshold, 3)) - (22.0776 * pow(threshold, 2)) + (132.1516 * threshold) - 0.1395;
+	vector<Point2f> dst_pts_out;
+	vector<Point2f> base_image_pts_out;
+
+	normalize(dst_pts_, dst_pts_out, 100, 0, NORM_L2, -1, noArray());
+	normalize(base_image_pts_, base_image_pts_out, 100, 0, NORM_L2, -1, noArray());
+
+	int dist = norm(base_image_pts_out, dst_pts_out, NORM_L2);
+	cout << "dist = " << dist << endl;
 
 	// Find homography (pixel != black)
-	//Mat h = findHomography(baseImagePts_, dstPts_, RANSAC, thresHold, noArray(), iterations);
-	//Mat h = findHomography(base_image_pts_, dst_pts_, RANSAC, 7);
-	Mat h = findHomography(base_image_pts_, dst_pts_, LMEDS);
-	//Mat h = findHomography(baseImagePts_, dstPts_, RHO, 5); // 4
+	//Mat h = findHomography(base_image_pts_, dst_pts_, RANSAC, dist);
+	//Mat h = findHomography(base_image_pts_, dst_pts_, LMEDS);
+	Mat h = findHomography(base_image_pts_, dst_pts_, RHO, dist);
 	//cout << endl << "homography = " << endl << h << endl << endl;
 
 	prev_offset_y_ = new_offset_y_;
-	cout << "pre_offSetY = " << prev_offset_y_ << endl;
+	cout << "pre_offSet_y_ = " << prev_offset_y_ << endl;
 	// Finding the needed canvas size 
-	offset_x_ = h.at<double>(0, 2);
-	offSetY = h.at<double>(1, 2);
-	//cout << "offSetX = " << offSetX << endl;
-	//cout << "offSetY = " << offSetY << endl;
+	int offSet_y_ = 0;
+	offSet_y_ = h.at<double>(1, 2);
+	cout << "offSet_y_ = " << offSet_y_ << endl;
 
-	new_offset_y_ = prev_offset_y_ + abs(offSetY);
-	cout << "new_offSetY = " << new_offset_y_ << endl;
+	new_offset_y_ = prev_offset_y_ + abs(offSet_y_);
+	cout << "new_offSet_y_ = " << new_offset_y_ << endl;
+	cout << "Warping perspective...." << endl;
 
 	// Use homography to warp image
 	Mat warpedImage;
@@ -55,15 +60,13 @@ void Warping::vector_split_(MatchedKeyPoint features) {
 
 	//cout << "pts_base_image = " << endl << baseImagePts_ << endl << endl;
 	//cout << "pts_dst = " << endl << dstPts_ << endl << endl;
-
-	num_features_ = features.image_1.size();
 	cout << "}" << endl;
 }
 
 
 // Translational warping
 Mat Warping::translate(Mat img, int offsetx, int offsety) {
-	//cout << "translate() {" << endl;
+	cout << "translate() {" << endl;
 
 	Mat trans_img;
 	Mat trans_mat = Mat::ones(offsety, offsetx, img.type());
@@ -72,10 +75,15 @@ Mat Warping::translate(Mat img, int offsetx, int offsety) {
 	//cout << "trans_mat size = " << trans_mat.size() << endl;
 
 	trans_mat = (Mat_<double>(2, 3) << 1, 0, offsetx, 0, 1, offsety);
-	warpAffine(img, trans_img, trans_mat, Size(img.cols + offsetx, img.rows + offsety));
 
-	//cout << "trans_img size = " << trans_img.size() << endl;
-	//cout << "}" << endl << endl;
+	try {
+		warpAffine(img, trans_img, trans_mat, Size(img.cols + offsetx, img.rows + offsety));
+	}
+	catch (const std::exception& e) {
+		cout << e.what() << endl;
+	}
+	cout << "trans_img size = " << trans_img.size << endl;
+	cout << "}" << endl;
 	return trans_img;
 }
 
