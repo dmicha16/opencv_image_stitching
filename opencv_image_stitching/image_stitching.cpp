da0b338f41b4ac4b8425e1d52d1aafa82be644cc
Svendsen92 <ssven16@student.aau.dk>
2018-05-20 14:02:56 +0200

    Implemented the inverted stitching

--- opencv_image_stitching/image_stitching.cpp
+++ opencv_image_stitching/image_stitching.cpp
@@ -12,6 +12,7 @@ int main() {
 
 	ADD_FILE("clogging.log");
 	cv::ocl::setUseOpenCL(false);
+	
 
 	Stitcher stitcher;
 	Warping warper;
@@ -44,10 +45,11 @@ int main() {
 		std::cout << "first image size - " << images_to_stitch[0].size << endl;
 		std::cout << "second image size - " << images_to_stitch[1].size << endl;
 
-		int offsetX = images_to_stitch[1].cols *1.25;
+		/*int offsetX = images_to_stitch[1].cols *1.5;
 		cout << "offsetX = " << offsetX << endl;
 		images_to_stitch[0] = warper.translate(images_to_stitch[0], offsetX, 0);
 		images_to_stitch[1] = warper.translate(images_to_stitch[1], offsetX, 0);
+		*/
 
 #pragma region output_current_images
 		//Mat img1, img2, raw;
@@ -66,55 +68,52 @@ int main() {
 		/************************************** FEATURES *************************************/
 
 		int rows = 3, columns = 3, desired_occupied_rect = 4;
-		float threshold = 0.5, image_overlap = 0.95;		
+		float threshold = 0.5, image_overlap = 0.35;		
 		finder.set_rectangle_info(rows, columns, image_overlap, desired_occupied_rect);
-		finder.find_features(images_to_stitch, threshold);
+		finder.set_images(images_to_stitch);
+		finder.find_features(threshold, i);
 		
 		MatchedKeyPoint matched_key_points = finder.get_matched_coordinates();
 
 		/******************************************* WARPING *******************************************/
 
-		Mat warped_img = warper.warp(images_to_stitch[0], matched_key_points);
-		/*Mat warp;
-		resize(warped_img, warp, cvSize(0, 0), 0.4, 0.4);
-		string warp_name = "warped image" + to_string(i);*/
-		//imshow(warp_name, warp);
+		Mat warped_img = warper.warp(images_to_stitch[1], matched_key_points);
+		
 		/****************************************** STITCHING *******************************************/
 
-		stitched_img = stitcher.customMerger(images_to_stitch[1], warped_img);
+		stitched_img = stitcher.customMerger(images_to_stitch[0], warped_img);
 
 		if (i < (raw_images.size() - 2)) {
 			images_to_stitch[0].release();
 			images_to_stitch[1].release();
 
+			/*
 			Mat reduced_stitched_img;
 			if (i == 0) {
-				cout << "image reduced by: " << offsetX * 0.5 << " cols" << endl;
-				reduced_stitched_img = stitched_img.colRange(0, stitched_img.cols - (offsetX*0.5));
+				double correction = 0.10;
+				cout << "image reduced by: " << offsetX * correction << " cols" << endl;
+				reduced_stitched_img = stitched_img.colRange(0, stitched_img.cols - (offsetX * correction));
 			}
 			else {
-				cout << "image reduced by: " << offsetX * 0.85 << " cols" << endl;
-				reduced_stitched_img = stitched_img.colRange(0, stitched_img.cols - (offsetX*0.85));
+				double correction = 0.85;
+				cout << "image reduced by: " << offsetX * correction << " cols" << endl;
+				reduced_stitched_img = stitched_img.colRange(0, stitched_img.cols - (offsetX * correction));
 			}
 
 			images_to_stitch[0] = reduced_stitched_img;
+			*/
+			images_to_stitch[0] = stitched_img;
 			images_to_stitch[1] = raw_images[i + 2];
 
+			String output_location = "../opencv_image_stitching/Images/Results/PROSAC11_intermediary#" + to_string(i+1) + "_0.5.jpg";
+			cv::imwrite(output_location, stitched_img);
+
 			stitched_img.release();
-			/*Mat inter;
-			resize(images_to_stitch[0], inter, cvSize(0, 0), 0.4, 0.4);
-			string int_name = "inter image" + to_string(i);*/
-			//imshow(int_name, inter);
-			//break;
 		}
-
 	}
-	//Mat stitched;
-	//resize(stitched_img, stitched, cvSize(0, 0), 0.01, 0.01);
-	//imshow("Stitched image", stitched);
-	String output_location = "../opencv_image_stitching/Images/Results/PROSAC_dist2.jpg";
-	cv::imwrite(output_location, stitched_img);
 
+	String output_location = "../opencv_image_stitching/Images/Results/PROSAC11_0.5.jpg";
+	cv::imwrite(output_location, stitched_img);
 
 	std::cout << endl << "------------ MISSION COMPLETE ------------" << endl;
 	WINPAUSE;
